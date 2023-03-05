c5e982020774a79d3c212db9a1b9c41f88e6c5c8
Diego Porras <diego.porras@owasp.org>
2023-03-05 23:05:41 +0000

    Add entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+
+#[panic_handler]
+// PanicInfo parameter contains the file and line
+// where the panic happened and the optional panic message
+
+// -> ! indicates a diverging function
+// (A function that do not retur)
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+//invoked directly by the operating system or bootloader.
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
