267052dcfbf709b12030d56625131b31be4ac4c7
Diego Porras <diego.porras@owasp.org>
2023-03-13 00:19:44 +0000

    Add library for testing

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
-mod vga_buffer;
-mod serial;
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
+use rust_os::println;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -31,10 +21,7 @@ fn panic(_info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    serial_println! ("[Failed]");
-    serial_println!("Error {}\n", _info);
-    exit_qemu(QemuExitCode::Failed);
-    loop{}
+    rust_os::test_panic_handler(_info);    
 }
 
 //invoked directly by the operating system or bootloader.
@@ -77,36 +64,5 @@ functions could lead to undefined behavior.
 #[test_case]
 fn trivial_assertion() {
     assert_eq!(1, 1);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-    unsafe {
-        //Magic number is the io port we declared in Cargo.toml
-        let mut port =  Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
 }
 
