8c48edb677f31ed5839203d706bf16d0447214bb
Diego Porras <diego.porras@owasp.org>
2023-04-07 03:07:34 +0100

    Load custom TSS and CS

--- src/gdt.rs
+++ src/gdt.rs
@@ -19,18 +19,29 @@ lazy_static! {
 }
 
 
-use x86_64::structures::gdt::{ GlobalDescriptorTable, Descriptor};
+use x86_64::structures::gdt::{ GlobalDescriptorTable, Descriptor, SegmentSelector};
 lazy_static! {
-    static ref GDT: GlobalDescriptorTable = {
+    static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt: GlobalDescriptorTable = GlobalDescriptorTable::new();
-        gdt.add_entry(Descriptor::kernel_code_segment());
-        let tss_descriptor = Descriptor::tss_segment(&TSS);
-        gdt.add_entry(tss_descriptor);
-        gdt
+        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let tss_segment = Descriptor::tss_segment(&TSS);
+        let tss_selector = gdt.add_entry(tss_segment);
+        (gdt, Selectors {code_selector, tss_selector})
     };
 
 }
+struct Selectors {
+    code_selector: SegmentSelector,
+    tss_selector: SegmentSelector
+}
 
 pub fn init(){
-    GDT.load();
+    GDT.0.load();
+    use x86_64::instructions::tables::load_tss;
+    use x86_64::instructions::segmentation::{CS, Segment};
+    unsafe {
+        CS::set_reg(GDT.1.code_selector);
+        load_tss(GDT.1.tss_selector);
+    }
+    
 }
